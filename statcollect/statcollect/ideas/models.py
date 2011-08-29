5f96398298770f9f06f3446040f85d3b9cb71ffd
Sanjay <svsaraf90@gmail.com>
2011-08-29 01:32:43 -0500

    facebook integration

---
+++ statcollect/statcollect/ideas/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+class UserMethods:
+    def name(self):
+        return self.first_name + " " + self.last_name
+
+User.__bases__ += (UserMethods,)
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    fbid = models.BigIntegerField(default=0, primary_key=True)
+#    friends = models.ManyToManyField("self")
+    access_token = models.CharField(max_length=200, default='')
+
+    def get_friends(self):
+        return self.friends.all()
+
+    def __unicode__(self):
+        return "%s" % self.user
+
+class Idea(models.Model):
+    author = models.ForeignKey(User, related_name="author_idea")
+    ideaname = models.CharField(max_length=30)
+    ideadescription = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    seen = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "%s created %s at %s" % (self.author, self.ideaname, self.created_at)
+
+class IdeaForm(ModelForm):
+    class Meta:
+        model = Idea
+
+
+    
+# Create your models here.
